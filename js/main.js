fb692387623f2dbecb35d1d4af199047f3d91bf5
MinoriKUNO <minori.fr@gmail.com>
2021-05-18 18:10:09 +0200

    updated 18 May

--- js/main.js
+++ js/main.js
@@ -5,14 +5,44 @@ let cercle = document.getElementsByClassName('cercle');
 let myPhoto = document.getElementsByClassName('myPhoto');
 // console.log(myPhoto[0]);
 
-cercle[0].addEventListener('click', ()=>{
-  myPhoto[0].style.backgroundImage ="url(images/minori_pic.jpg)";
+cercle[0].addEventListener('click', () => {
+  myPhoto[0].style.backgroundImage = "url(images/minori_pic.jpg)";
 
 })
-cercle[0].addEventListener('dblclick', ()=>{
-  myPhoto[0].style.backgroundImage ="url(images/main_photo.png)";
+cercle[0].addEventListener('dblclick', () => {
+  myPhoto[0].style.backgroundImage = "url(images/main_photo.png)";
 
 })
 
 // Affiché le heading corrésponds aux buttons
 let heading = document.getElementById('heading');
+let me = document.getElementById('me');
+let parcours = document.getElementById('parcours');
+let cv = document.getElementById('cv');
+let linkedin = document.getElementById('linkedin');
+
+// Object message : btn name
+let messageH = {
+  "Me découvrir": me,
+  "Mon parcours": parcours,
+  "Mon CV": cv,
+  "Mon compte Linkedin": linkedin
+}
+
+// display object array in JS with Object.keys()
+Object.keys(messageH).forEach(messeage => {
+  // default message
+  messageH[messeage].addEventListener('mouseleave', () => {
+    heading.innerText = "Cliquer les touches :";
+  });
+  // Message correnponds to btn
+  messageH[messeage].addEventListener('mouseover', () => {
+    heading.innerText = messeage;
+  });
+
+})
+
+
+
+
+
